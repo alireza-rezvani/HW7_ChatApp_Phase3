7d9be467061ee8fe0b8493315df0d62c67d07643
arfjava <arrfjava@gmail.com>
2019-12-20 19:02:07 +0330

    ClientDemo classes completed.

---
+++ src/ir/maktab32/java/projects/homework7/chatapp3/client/ClientDemo.java
@@ -0,0 +1,7 @@
+package ir.maktab32.java.projects.homework7.chatapp3.client;
+
+public class ClientDemo {
+    public static void main(String[] args) {
+        Login.execute();
+    }
+}
---
+++ src/ir/maktab32/java/projects/homework7/chatapp3/client/ClientDemo2.java
@@ -0,0 +1,7 @@
+package ir.maktab32.java.projects.homework7.chatapp3.client;
+
+public class ClientDemo2 {
+    public static void main(String[] args) {
+        Login.execute();
+    }
+}
